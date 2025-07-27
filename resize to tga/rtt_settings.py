12319a445d887e6ad2b21f2f28c0605b595a2606
Esoteric <161647400+EnlightenedEsoteric@users.noreply.github.com>
2025-07-27 18:49:43 +0100

    Updated rtt_settings.py because I forgot a single letter "r"

--- resize to tga/rtt_settings.py
+++ resize to tga/rtt_settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-flag_folder = Path('.../gfx/flags') # Change this to where you want the created files to be placed (use '/' instead of '\')
+flag_folder = Path(r'.../gfx/flags') # Change this to where you want the created files to be placed (use '/' instead of '\')
 
 output_sizes = ((10,7),(41,26),(82,52)) # Change this to the dimensions of the TGA files generated, one file will be created for each pair of dimensions
 # Small - 10x7
